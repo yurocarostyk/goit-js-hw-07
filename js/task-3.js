6d43122a497672d9d9937c2e43af14c32eaa8652
yurocarostyk <152785136+yurocarostyk@users.noreply.github.com>
2024-04-17 06:32:41 +0200

    task

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', event => {
+  if (event.target.value.trim() == '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = event.currentTarget.value.trim();
+  }
+});
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const form = e.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email.trim() === '' || password.trim() === '') {
+    alert('All form fields must be filled in');
+  }
+
+  if (email.trim() != '' && password.trim() != '') {
+    const result = {
+      email: email.trim(),
+      password: password.trim(),
+    };
+    console.log(result);
+    loginForm.reset();
+  }
+});
